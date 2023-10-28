c05ab1312731f0b31ff7149f2199f82d7f77a4fb
darelljay <darelljason1@gmail.com>
2023-10-28 22:15:17 +0900

    ......

--- 1.php
+++ 1.php
@@ -14,20 +14,62 @@
         font-family: sans-serif;
     }
     .container{
-        height: 24px;
+        height: 24rem;
         width: 100%;
         display: flex;
         justify-content: center;
         align-items: center; 
     }
+    .holder{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .hero{
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+    }
+    .holder input[type="text"]{
+        height: 30px;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-size: 20px;
+        border-bottom: 2px solid green;
+    }
+    .holder input[type="submit"]{
+        display: block;
+        height: 32px;
+        font-size: 20px;
+        color: #fff;
+        border: 1px solid;
+        width: 250px;
+        outline: none;
+        background-color: crimson;
+    }
+    .footer{
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
 <body>
     <h1 class="hero">
-        The user's name is 🚀🚀🔥🔥
+        <?php
+        if(isset($_POST['submit'])){
+            echo "The user's name is ". $_POST['name'] ."🚀🚀🔥🔥";
+        }else{
+            echo "Enter your name";
+        }
+        ?>
     </h1>
     <div class="container">
         <div class="holder">
-            <form method="post" action="index.php">
+            <form method="post" action="1.php">
                 <div class="wrapper">
                     <input type="text" placeholder="Enter Username" name="name">
                 </div>
